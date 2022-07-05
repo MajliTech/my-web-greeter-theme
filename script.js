eab1a4292d206a45b657fe678a9477893ed2fa76
BuildTools <unconfigured@null.spigotmc.org>
2022-07-05 11:00:36 +0200

    Upload :)

---
+++ script.js
@@ -0,0 +1,65 @@
+// function signin() {
+//     "bout to signin with "+document.querySelector(".user").value+" and pass "+document.querySelector(".passw").value
+//     lightdm.authenticate(document.querySelector(".user").value)
+//     lightdm.respond(document.querySelector(".passw").value)
+//     lightdm.start_session("")
+// }
+gup = (key) => {
+  address = window.location.search
+  parameterList = new URLSearchParams(address)
+  return parameterList.get(key)
+}
+function loginUsr(user) {
+  // Update visual things
+
+  lightdm.cancel_authentication()
+  lightdm.authenticate(user.username)
+}
+function getPicOf(user) {
+  for (let i in lightdm.users) {
+    if (lightdm.users[i].username == user && lightdm.users[i].image){
+      return lightdm.users[i].image
+    }
+  }
+  return "placeholder.png"
+}
+
+function reqPass(user, name, logged_in) {
+  if (logged_in) {
+    window.location.href = "login.html?user=" + user.username + "&name=" + name + "&lock=true"
+  } else { window.location.href = "login.html?user=" + user.username + "&name=" + name + "&lock=false" }
+}
+function setup() {
+  document.querySelector("#eraseme").innerHTML = "";
+  let users = lightdm.users;
+  if (gup("return") != "true") {
+    for (let i in users) {
+      if (users[i].logged_in) {
+        window.location.href = "login.html?user=" + users[i].username + "&name=" + users[i].display_name + "&lock=true"
+      }
+    }
+  }
+  for (let i = 0; i < users.length; i++) {
+    var button = document.createElement("button")
+    button.innerHTML = "<img style=height:43px;border-radius:100%;margin-right:5px src='"+getPicOf(users[i].username)+"'>"+users[i].display_name;
+    button.addEventListener("click", () => {
+      reqPass(users[i], users[i].display_name, users[i].logged_in)
+
+    })
+    document.querySelector("#users").appendChild(button)
+  }
+  if (lightdm.has_guest_account) {
+    var button = document.createElement("button")
+    button.innerHTML = "Guest session";
+    button.addEventListener("click", () => {
+      lightdm.authenticate_as_guest()
+      setTimeout(() => { lightdm.start_session("pantheon") }, 500)
+    }); document.querySelector("#users").appendChild(button)
+  }
+  var button = document.createElement("button")
+  button.innerHTML = "Shutdown options";
+  button.addEventListener("click", () => {
+    window.location.href = "shutdown.html"
+  }); document.querySelector("#users").appendChild(button)
+}
+window.addEventListener("GreeterReady", setup);
---
+++ scriptlogin.js
@@ -0,0 +1,60 @@
+gup = (key) => {
+  
+  // Address of the current window
+  address = window.location.search
+
+  // Returns a URLSearchParams object instance
+  parameterList = new URLSearchParams(address)
+
+  // Returning the respected value associated
+  // with the provided key
+  return parameterList.get(key)
+}
+lightdm.authentication_complete.connect(() => {
+  if (lightdm.is_authenticated) {
+  document.querySelector("#login").style="background-color:green";
+  setTimeout(() => {lightdm.start_session("pantheon");},500)} else {
+    document.querySelector("#login").style="background-color:red";
+  }
+})
+function getBackOf(user) {
+  for (let i in lightdm.users) {
+    if (lightdm.users[i].username == user && lightdm.users[i].background){
+      return lightdm.users[i].background
+    }
+  }
+  return "placeholder.png"
+}
+function getPicOf(user) {
+  for (let i in lightdm.users) {
+    if (lightdm.users[i].username == user && lightdm.users[i].image){
+      return lightdm.users[i].image
+    }
+  }
+  return "placeholder.png"
+}
+function loginUsr() {
+  var username = gup("user");
+  lightdm.cancel_authentication()
+  lightdm.authenticate(username)
+  
+  setTimeout(() => {lightdm.respond(document.querySelector("#pass").value)},200)
+  document.querySelector("#login").style="background-color:yellow"
+}
+function prepare(){
+  let users = lightdm.users
+  var username = gup("user");
+  var name = gup("name");
+  document.querySelector(".name").innerHTML = decodeURI(name);
+  document.querySelector("#pp").src = getPicOf(username);
+  document.body.style.backgroundImage = "url('"+getBackOf(username)+"')"
+  if (gup("lock")=="true") {
+    document.querySelector("#islocked").innerHTML = "Locked session"
+  }
+}
+function clickPress(event) {
+  if (event.keyCode == 13) {
+  loginUsr();
+  }
+}
+window.addEventListener("GreeterReady", prepare);
