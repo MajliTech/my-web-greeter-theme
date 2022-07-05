27ef46a1a85dda376b36a0287d414d3d23a61775
BuildTools <unconfigured@null.spigotmc.org>
2022-07-05 13:22:06 +0200

    Final commit for the day :)

--- scriptother.js
+++ scriptother.js
@@ -13,10 +13,53 @@ gup = (key) => {
 lightdm.authentication_complete.connect(() => {
   if (lightdm.is_authenticated) {
   document.querySelector("#login").style="background-color:green";
-  setTimeout(() => {lightdm.start_session(lightdm.sessions[0].key);},500)} else {
+    var username = document.querySelector("#user").value
+    if (islocked(username)) {
+      document.querySelector("#islocked").innerHTML = "Locked session"
+    }
+    document.body.style.backgroundImage = "url('"+getBackOfEnd(username)+"')";  
+    document.querySelector(".name").innerHTML = getNameOf(username);
+    document.querySelector("#pp").src = getPicOfEnd(username);
+    setTimeout(() => {
+      lightdm.start_session(lightdm.sessions[0].key)},800);
+    } else {
     document.querySelector("#login").style="background-color:red";
   }
 })
+function getNameOf(user){
+  for (let i in lightdm.users) {
+    if (lightdm.users[i].username == user && lightdm.users[i].display_name){
+      return lightdm.users[i].display_name
+    }
+  }
+  return "Other account"
+}
+
+function islocked(user) {
+  for (let i in lightdm.users) {
+    if (lightdm.users[i].username==user){
+      return lightdm.users[i].logged_in
+    }
+    
+  }
+  return false
+}
+function getBackOfEnd(user) {
+  for (let i in lightdm.users) {
+    if (lightdm.users[i].username == user && lightdm.users[i].background){
+      return lightdm.users[i].background
+    }
+  }
+  return "odin-dark.jpg"
+}
+function getPicOfEnd(user) {
+  for (let i in lightdm.users) {
+    if (lightdm.users[i].username == user && lightdm.users[i].image){
+      return lightdm.users[i].image
+    }
+  }
+  return "placeholder.png"
+}
 function getBackOf(user) {
   return "odin-dark.jpg"
 }
